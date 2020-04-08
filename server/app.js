a474597dcc78a3e5c6176eaa5c59dedbd4110855
Ruesi <the_ruesi@zoho.com>
2020-04-08 15:40:49 +0100

    added / to * route

--- server/app.js
+++ server/app.js
@@ -5,14 +5,9 @@ const express = require('express');
 
 const app = express(); 
 
-
-
-console.log('dirname is ', __dirname);
-
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.get('*', (req, res) => {
-  console.log('this did run')
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../', 'public', 'index.html'))
 })
 
